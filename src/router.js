7811f413194d773674abcd7e721126be7536a95c
Justin Navarro <info@justinnavarro.net>
2019-07-25 00:36:12 -0400

    installed vuetify and setup home component

--- src/router.js
+++ src/router.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/pages/home/Home'
 
 Vue.use(Router)
 
 export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes: []
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+        },
+    ]
 })
