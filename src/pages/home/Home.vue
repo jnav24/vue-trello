e477c17579e93e2779da89c2bdd920633f152f5b
Justin Navarro <info@justinnavarro.net>
2019-07-25 20:49:40 -0400

    markup to display trello cards

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -8,7 +8,35 @@
 					Select board to display cards
 				</div>
 
-				<pre>{{ allCards }}</pre>
+				<div v-if="!isEmpty(getSelected.board) && !allCards.length">
+					This board has no cards
+				</div>
+
+				<div v-if="!isEmpty(getSelected.board) && allCards.length">
+					<h2>Showing {{ allCards.length }} Cards</h2>
+
+					<v-container grid-list-md style="padding-left: 0;padding-right: 0;">
+						<v-layout justify-space-evenly row wrap>
+							<v-flex
+								lg4 xl4
+								:key="card.id"
+								v-for="(card) in allCards">
+								<v-card>
+									<v-card-text style="text-align: left">
+										<pre>{{ card }}</pre>
+										<h3 style="height: auto; max-height: 50px; overflow: hidden; text-overflow: ellipsis;white-space: nowrap; width: 100%;">{{ card.name }}</h3>
+										<p>{{ card.desc }}</p>
+										<v-chip
+											:key="int"
+											v-for="(label, int) in card.idLabels">
+											{{ getLabelName(label) }}
+										</v-chip>
+									</v-card-text>
+								</v-card>
+							</v-flex>
+						</v-layout>
+					</v-container>
+				</div>
 			</v-flex>
 		</v-layout>
 	</div>
@@ -25,6 +53,7 @@
 		computed: {
 			...mapGetters([
 				'allCards',
+				'allLabels',
 				'getSelected'
 			]),
 		},
@@ -34,6 +63,13 @@
 		methods: {
 			isEmpty(value) {
 				return !value && !value.trim()
+			},
+			getLabelName(label) {
+				const obj = this.allLabels.find((lab) => lab.id === label)
+
+				if (typeof obj !== 'undefined' && typeof obj.name !== 'undefined') {
+					return obj.name
+				}
 			},
 		}
 	}
